5ee5ca81b587010c64bae29d36ea069dcaa59842
ashams008 <ashams001@gmail.com>
2025-05-14 14:23:33 +0530

    updated db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     MYSQL_HOST = '3.219.173.225'
     MYSQL_USER = 'pn_admin_Usr'
     MYSQL_PASSWORD = 'HtsPool2024'
-    MYSQL_PN_DB = 'plantnavigator'
+    MYSQL_DB = 'plantnavigator'
     MYSQL_PORT = 3306
     #email configuration
     SECRET_KEY = 'HTS_S@@rgummi@2025'
