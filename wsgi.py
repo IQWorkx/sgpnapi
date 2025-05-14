c2d46b21e5877841b216f34038003c57e9b5ded3
ashams008 <ashams001@gmail.com>
2025-05-14 13:15:04 +0530

    app

--- wsgi.py
+++ wsgi.py
@@ -4,24 +4,20 @@ from app.routes import register_routes
 from config import Config
 from app.routes.auth_routes import auth_bp   
 
-app = create_app()
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+jwt.init_app(app)
+
+# with app.app_context():
+#     from app.models import user, product
+#     db.create_all()
+
+register_routes(app)
+app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
-    app.run()
+      app.run(debug=True,  port=5000)
 # Gunicorn command to run the app
 # gunicorn --bind
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    db.init_app(app)
-    jwt.init_app(app)
-
-    # with app.app_context():
-    #     from app.models import user, product
-    #     db.create_all()
-
-    register_routes(app)
-    app.register_blueprint(auth_bp)
-
-    return app
