08149ac706b1d12fae5040f77312f733bc35acfe
ashams008 <ashams001@gmail.com>
2025-05-14 12:47:47 +0530

    added pn routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from .user_routes import user_bp
 from .product_routes import product_bp
+from .pn_routes import pn
 
 def register_routes(app):
     app.register_blueprint(user_bp)
     app.register_blueprint(product_bp)
+    app.register_blueprint(pn)
--- app/routes/pn_routes.py
+++ app/routes/pn_routes.py
@@ -1,14 +1,8 @@
 from flask import Blueprint, jsonify, request
-# from app.models.product import Product
 from app.extensions import db
 from flask import render_template
 
 pn_bp = Blueprint('pn', __name__, url_prefix='/pn')
-
-@product_bp.route('/', methods=['GET'])
-def get_products():
-    products = Product.query.all()
-    return jsonify([{'id': p.id, 'title': p.title} for p in products])
 
 @pn.route('/', methods=['GET'])
 def index():
