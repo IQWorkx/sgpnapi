3952a5fffce039ac764487fa95d0b3497b8cdc2e
ashams008 <ashams001@gmail.com>
2025-05-14 12:52:22 +0530

    DB CHanges

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from .extensions import db, jwt
-from .routes import register_routes
+from app.routes import register_routes
 from .config import Config
-from .routes.auth_routes import auth_bp
+from app.routes.auth_routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
@@ -11,9 +11,9 @@ def create_app():
     db.init_app(app)
     jwt.init_app(app)
 
-    with app.app_context():
-        from app.models import user, product
-        db.create_all()
+    # with app.app_context():
+    #     from app.models import user, product
+    #     db.create_all()
 
     register_routes(app)
     app.register_blueprint(auth_bp)
---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+from .product import Product
+
+# This file is used to initialize the models package.
+# Import all models here to make them available for use.
+
+
+__all__ = ["User", "Product"]
